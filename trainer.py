17a08a21a6f216ca7135aab766fc42df924ee1df
Maxime Fournes <mf@seldon.io>
2016-12-15 19:12:15 +0000

    Improved RL trainer

--- trainer.py
+++ trainer.py
@@ -1,4 +1,12 @@
 from subprocess import call
+import os
+import time
+
+n_hlt_to_keep = 100
 
 while True:
-	result = call(['./halite', '-t', '-d', '15 15', 'python RLBot1.py', 'python3 RefactoredBot.py'])
+	result = call(['./halite_mod', '-t', '-d', '15 15', 'python RLBot1.py', 'python3 RandomBot2.py'])
+	hlt_files = [x for x in os.listdir('.') if x[-4:]=='.hlt']
+	while len(hlt_files) > n_hlt_to_keep:
+		os.remove(hlt_files.pop(0))
+	time.sleep(3)
